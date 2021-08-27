2f7d35a724f99a2ab0bdd2c3fef02f505fff7e0e
Syukib <89130000+Syukib@users.noreply.github.com>
2021-08-26 21:51:03 -0400

    Update htmlunit_test.java
    removing the append option to the result file

--- src/main/java/com/crawler/test/htmlunit_test.java
+++ src/main/java/com/crawler/test/htmlunit_test.java
@@ -34,10 +34,10 @@ public class htmlunit_test {
 	        if (!Files.exists(lifecycle_result_data)) {
 	            Files.createFile(lifecycle_result_data);
 	        }
-	        Files.write(lifecycle_result_data, version_name.getBytes(), StandardOpenOption.APPEND);
-	        Files.write(lifecycle_result_data, " , ".getBytes(), StandardOpenOption.APPEND);
-	        Files.write(lifecycle_result_data, eol.getBytes(), StandardOpenOption.APPEND);
-	        Files.write(lifecycle_result_data, "\n".getBytes(), StandardOpenOption.APPEND);
+	        Files.write(lifecycle_result_data, version_name.getBytes());
+	        Files.write(lifecycle_result_data, " , ".getBytes());
+	        Files.write(lifecycle_result_data, eol.getBytes());
+	        Files.write(lifecycle_result_data, "\n".getBytes());
 		}		
 	}
 	
